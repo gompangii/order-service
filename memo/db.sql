52507b08886e5d692ef827a47443c3c9c42285aa
Gompangii <gompangii@gmail.com>
2025-02-26 17:39:10 +0900

    12.4.3 스프링 보안 및 스프링 데이터 R2DBC로 사용자 데이터 보호

---
+++ memo/db.sql
@@ -0,0 +1,16 @@
+# 보안을 위한 스키마 변경(mariadb 기준)
+DROP TABLE orders;
+
+CREATE TABLE orders (
+    id                  INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	book_isbn           VARCHAR(255) NOT NULL,
+	book_name           VARCHAR(255),
+	book_price          DOUBLE NOT NULL,
+	quantity  			INT,
+	`status`  			VARCHAR(255) NOT NULL,
+	created_date        TIMESTAMP NOT NULL,
+    last_modified_date  TIMESTAMP NOT NULL,
+    created_by 			VARCHAR(255),
+	last_modified_by    VARCHAR(255),
+	`version`           INT NOT NULL
+);
