66d1f3d00cfb8d71ea0ef3cf131d2c8487745d00
Gompangii <gompangii@gmail.com>
2025-03-06 13:40:46 +0900

    12.4.4 @WithMockUser 및 스프링 데이터 R2DBC를 통한 데이터 감사 및 보호 테스트

--- src/test/java/com/polarbookshop/orderservice/book/BookClientTests.java
+++ src/test/java/com/polarbookshop/orderservice/book/BookClientTests.java
@@ -2,9 +2,7 @@ package com.polarbookshop.orderservice.book;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,7 +12,9 @@ import reactor.test.StepVerifier;
 
 import java.io.IOException;
 
+@TestMethodOrder(MethodOrderer.class)
 class BookClientTests {
+
   private MockWebServer mockWebServer;
   private BookClient bookClient;
 
@@ -22,6 +22,7 @@ class BookClientTests {
   void setup() throws IOException {
     this.mockWebServer = new MockWebServer();
     this.mockWebServer.start();
+
     var webClient = WebClient.builder()
       .baseUrl(mockWebServer.url("/").uri().toString())
       .build();
@@ -56,6 +57,21 @@ class BookClientTests {
     StepVerifier.create(book)
       .expectNextMatches(b -> b.isbn().equals(bookIsbn))
       .verifyComplete();
-
   }
+
+  @Test
+  void whenBookNotExistsThenReturnEmpty() {
+    var bookIsbn = "1234567891";
+
+    var mockResponse = new MockResponse()
+      .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+      .setResponseCode(404);
+
+    mockWebServer.enqueue(mockResponse);
+
+    StepVerifier.create(bookClient.getBookByIsbn(bookIsbn))
+      .expectNextCount(0)
+      .verifyComplete();
+  }
+
 }
