16c20145025bd287c01c6a436b2d4eae285e6690
Gompangii <gompangii@gmail.com>
2025-03-06 16:58:50 +0900

    13.1.1 스프링 부트를 사용한 로깅

--- src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
@@ -3,6 +3,8 @@ package com.polarbookshop.orderservice.order.web;
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,6 +12,8 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("orders")
 public class OrderController {
+
+  private static final Logger log = LoggerFactory.getLogger(OrderController.class);
   private final OrderService orderService;
 
   public OrderController(OrderService orderService) {
@@ -18,11 +22,13 @@ public class OrderController {
 
   @GetMapping
   public Flux<Order> getAllOrders() {
+    log.info("Fetching all orders");
     return orderService.getAllOrders();
   }
 
   @PostMapping
   public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+    log.info("Order for {} copies of the book with ISBN {}", orderRequest.quantity(), orderRequest.isbn());
     return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
   }
 }
