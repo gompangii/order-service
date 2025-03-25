4dcca6bc74167f748859769c51afbce68a3ed7f6
Gompangii <gompangii@gmail.com>
2025-03-25 12:47:35 +0900

    catalog-service 연계 설정 오류 수정

--- memo/db.sql
+++ memo/db.sql
@@ -5,7 +5,7 @@ CREATE TABLE orders (
     id                  INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	book_isbn           VARCHAR(255) NOT NULL,
 	book_name           VARCHAR(255),
-	book_price          DOUBLE NOT NULL,
+	book_price          DOUBLE,
 	quantity  			INT,
 	`status`  			VARCHAR(255) NOT NULL,
 	created_date        TIMESTAMP NOT NULL,
